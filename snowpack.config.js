dac4dc1e0522a6614ddf3259eae979742a669f15
Hyblocker <bryan.spiteri@gmail.com>
2021-12-15 14:51:16 +0100

    Redo styling

--- snowpack.config.js
+++ snowpack.config.js
@@ -3,10 +3,11 @@
 
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
-  alias: {
-    $: './src',
-    $components: './src/components',
-    $content: './public/content',
-    $layouts: './src/layouts',
-  },
+    alias: {
+        $: './src',
+        $components: './src/components',
+        $content: './public/content',
+        $layouts: './src/layouts',
+        $assets: './src/assets',
+    },
 };
